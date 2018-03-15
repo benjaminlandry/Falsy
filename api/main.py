74f640deee4259c214e8a3d2504b82846fc84602
benjaminlandry <benjamin.landry@mail.mcgill.ca>
2018-03-15 11:12:57 -0400

    worker docker-compose & dockerfile | script can be called with docker-compose & docker container now remains open

--- api/main.py
+++ api/main.py
@@ -3,4 +3,4 @@ from falsy.falsy import FALSY
 f = FALSY()
 f.swagger('spec1.yml', ui=True, ui_language='en', cors_origin='*', theme='normal')
 # f.swagger('spec2.yml', ui=True, ui_language='zh-cn', theme='normal')
-api = f.api
+#api = f.api
--- setup_dockerNetwork.sh
+++
@@ -1,3 +0,0 @@
-
-sudo echo $mongoid $newMongoIP >> /etc/resolv.conf
-sudo echo $apiid $apiIP >> /etc/resolv.conf
